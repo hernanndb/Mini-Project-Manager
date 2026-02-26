c7092d66805cacf24dd10637c90fbc6307b104b2
hernanndb <hernann.db64@gmail.com>
2026-02-25 22:36:27 -0300

    Creacion de la bbdd en Postgres y Conexion del projecto cliente // bbdd

---
+++ src/Routes/auth.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/",)
+router.get("/login",)
+router.get("/logout",)
+router.get("/register",)
+
+
+router.post("/login" , );
+router.post("/register" ,);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import  express  from "express";
 import  server  from "./server.js"
+import  router from "./Routes/auth.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+//Configuracion de Carpeta raiz Public
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));                   
+app.use(express.static(path.join(__dirname, "public")));
 
 server(app);
